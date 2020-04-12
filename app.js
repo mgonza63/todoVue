448499f6a8f9019fc289599c20e24e48b8b45645
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-12 17:39:19 -0500

    created form and vue methods that log the value of user's input

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        title: 'Todo-App with Vue.js',
+        newTodo: '',
+        // create an empty array to push todos into
+        todos: [],
+    },
+    // object where functions go
+    methods: {
+        addTodo() {
+            // use 'this' to refer to the instance and access the data above
+            // push an object that includes the 'todo' and 'done' for future use
+            this.todos.push({
+                title: this.newTodo,
+                done: false,
+
+            })
+        }
+
+    }
+})
